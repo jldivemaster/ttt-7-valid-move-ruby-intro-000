50aee1d7e79229e5fffd474877807023af7bb52e
Jason Leach <jlspursfan@gmail.com>
2019-04-22 21:13:00 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -4,11 +4,11 @@ def valid_move?(board, index)
     return false
   end
 
-  if (board[index] == " ") || (board[index] == "") || (board[index] == nil)
+  if (board[index] == "X") || (board[index] == "O")
     return false
   end
 
-  if ((0..8).include?(index)) && (board[index] == "X") || (board[index] == "O")
+  if ((0..8).include?(index)) && ((board[index] == " ") || (board[index] == "") || (board[index] == nil))
     return true
   end
 end
